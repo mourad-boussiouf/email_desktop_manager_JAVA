2bf7da28228f6ca61b4230acb41daf50014cf6c7
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2023-04-24 17:28:38 +0200

    test random safe au niveau crytographique, bytetochar test WIP

--- src/emailapp/CryptoSafeRandom.java
+++ src/emailapp/CryptoSafeRandom.java
@@ -2,6 +2,9 @@ package emailapp;
 import java.security.SecureRandom;
 
 public class CryptoSafeRandom {
+
+    byte byteCharCode = 65;
+    char charFromByte = (char)byteCharCode;
     int randomInt = secureRandom.nextInt();
     long randomLong = secureRandom.nextLong();
     float randomFloat = secureRandom.nextFloat();
