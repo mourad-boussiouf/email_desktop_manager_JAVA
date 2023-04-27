e6fa53b408275589de8db2e07c626db17bf91f73
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2023-04-27 15:54:07 +0200

    test nextint de la classe SecureRandom OK

--- src/emailapp/CryptoSafeRandom.java
+++ src/emailapp/CryptoSafeRandom.java
@@ -9,14 +9,24 @@ import java.util.logging.Logger;
 public class CryptoSafeRandom {
 
     public CryptoSafeRandom() throws NoSuchAlgorithmException, NoSuchProviderException {
+
         SecureRandom secureRandomGenerator = SecureRandom.getInstance("SHA1PRNG", "SUN");
+        SecureRandom random = new SecureRandom();
+        byte seed[] = random.generateSeed(20);
+        byte seed2[] = random.generateSeed(40);
 
-        byte[] values = new byte[300];
-        SecureRandom a =  new SecureRandom();
-        a.nextBytes(values);
+        byte bytes[] = new byte[20];
+        byte bytes2[] = new byte[20];
 
-        System.out.println("TEST CRYPTO SAFE RANDOM: ");
-        System.out.println(values);
+        SecureRandom rand = new SecureRandom();
+
+        // Generate random integers in range 0 to 999
+        int rand_int1 = rand.nextInt(1000);
+        int rand_int2 = rand.nextInt(1000);
+
+        // Print random integers
+        System.out.println("Random Integers: " + rand_int1);
+        System.out.println("Random Integers: " + rand_int2);
     }
     byte byteCharCode = 65;
     char charFromByte = (char)byteCharCode;
