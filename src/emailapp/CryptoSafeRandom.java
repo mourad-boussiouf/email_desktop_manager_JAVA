e750bf0b39055799e802187fb18015999053d692
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2023-04-28 16:06:32 +0200

    enchainement try & catch pour écrire de belles erreurs

--- src/emailapp/CryptoSafeRandom.java
+++ src/emailapp/CryptoSafeRandom.java
@@ -1,5 +1,6 @@
 package emailapp;
 import java.security.NoSuchProviderException;
+import java.security.ProviderException;
 import java.util.Random;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
@@ -10,42 +11,44 @@ import java.util.logging.Logger;
 public class CryptoSafeRandom {
 
     public CryptoSafeRandom() throws NoSuchAlgorithmException, NoSuchProviderException {
-
-        SecureRandom secureRandomGenerator = SecureRandom.getInstance("SHA1PRNG", "SUN");
         SecureRandom rand = new SecureRandom();
-        SecureRandom random = new SecureRandom();
-        SecureRandom random1 = new SecureRandom();
-
         int rand_int1 = rand.nextInt(20);
-        int rand_int2 = rand.nextInt(20);
-        System.out.println("Random Integer1" + rand_int1);
-        System.out.println("Random Integer2" + rand_int2);
-
-        random1.setSeed(random1.generateSeed(rand_int1));
-        random1.nextBytes(new byte[20]);
-
-        System.out.println("random1.setSeed(random1.generateSeed(rand_int1));\n" +
-                "        random1.nextBytes(new byte[20]): " + new byte[20]);
-
-
-        byte bytes[] = new byte[20];
-        byte bytes2[] = new byte[20];
-
-
         String rdm = "";
         for (int i = 0; i < 10; i++) {
-            //s.push(rand.nextBytes(bytes));
             int rand_int3 = rand.nextInt(20);
+            byte[] bytes = new byte[rand_int3]
             Stack<String> s = new Stack<String>();
-            random.nextBytes(bytes);
+            rand.nextBytes(bytes);
             System.out.println("Random bytes: "+ bytes);
         }
+        try {
+            // creating the object of SecureRandom
+            SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
 
+            // Declaring the string variable
+            String str = "Tajmahal";
 
+            // Declaring the byte Array
+            // converting string into byte
+            byte[] b = str.getBytes();
 
+            // printing the byte array
+            System.out.println("Byte array before operation : " + Arrays.toString(b));
+
+            // generating user-specified number of random bytes
+            // using nextBytes() method
+            sr.nextBytes(b);
+
+            // printing the new byte array
+            System.out.println("Byte array after operation : " + Arrays.toString(b));
+        }
+
+        catch (NoSuchAlgorithmException e) {
+            System.out.println("Exception thrown : " + e);
+        }
+        catch (ProviderException e) {
+            System.out.println("Exception thrown : " + e);
+        }
     }
-    byte byteCharCode = 65;
-    char charFromByte = (char)byteCharCode;
-    byte[] values = new byte[124];
 
 }
