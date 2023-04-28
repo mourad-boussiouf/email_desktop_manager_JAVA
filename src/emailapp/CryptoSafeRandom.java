e39ca01585053fbeffff5dbfa2b59a8ca3661bfd
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2023-04-28 16:25:52 +0200

    generation de la string seed aléatoire avant d'utiliser méthode nextByte

--- src/emailapp/CryptoSafeRandom.java
+++ src/emailapp/CryptoSafeRandom.java
@@ -10,9 +10,18 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 public class CryptoSafeRandom {
 
-    public CryptoSafeRandom() throws NoSuchAlgorithmException, NoSuchProviderException {
+    public CryptoSafeRandom(int length) throws NoSuchAlgorithmException, NoSuchProviderException {
         SecureRandom rand = new SecureRandom();
         int rand_int1 = rand.nextInt(20);
+
+
+        String seedSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+{}[]-=";
+        char[] seedWord = new char[length];
+        for(int i=0; i<length; i++){
+            int rand_int = rand.nextInt(seedSet.length());
+            seedWord[i] = seedSet.charAt(rand_int);
+        }
+
         String rdm = "";
         for (int i = 0; i < 10; i++) {
             int rand_int3 = rand.nextInt(20);
@@ -22,14 +31,15 @@ public class CryptoSafeRandom {
             System.out.println("Random bytes: "+ bytes);
         }
         try {
-            // creating the object of SecureRandom
+            // initialisation objet secure random
             SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
 
-            // Declaring the string variable
-            String str = "Tajmahal";
-
-            // Declaring the byte Array
-            // converting string into byte
+            // declarer la seed
+            String str = new String(seedWord);
+            System.out.println("SEED WORD" + str);
+            return;
+            // déclarer le tableau de byte
+            // remplir le tableau de byte par le seedword aleatoire converti en byte
             byte[] b = str.getBytes();
 
             // printing the byte array
